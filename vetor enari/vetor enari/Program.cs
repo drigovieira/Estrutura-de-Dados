1614edaed966a5b34abd58af1f6f2f476ff1de0e
Jhonatan Luiz <47275727+SotnDragon@users.noreply.github.com>
2019-09-13 14:31:23 -0300

    Add files via upload

---
+++ vetor enari/vetor enari/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetor_enari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            // menu mmmm = new menu();
+
+            // mmmm.MENU();
+
+            
+
+            vetor v = new vetor();
+
+            for (int i = 0; i < 3; i++)
+            { 
+                Console.WriteLine("-----------");
+                Console.WriteLine("Criando");
+                v.addUser(i);
+               
+            }
+
+            v.editUser(0);
+
+           v.delUser(2);
+
+            
+            Console.WriteLine("Deletado");
+            v.listar();
+            Console.WriteLine("---------------");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ vetor enari/vetor enari/vetor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetor_enari
+{
+
+    class vetor
+    {
+
+        private usuarios[] pessoas = new usuarios[3];
+
+
+
+
+        public bool addUser(int index)
+        {
+            usuarios us = new usuarios();
+
+            Console.Write("\nEntre com o Nome: ");
+            us.setNome(Console.ReadLine());
+            Console.Write("Entre com o Email: ");
+            us.setEmail(Console.ReadLine());
+            Console.Write("Entre com a senha: ");
+            us.setSenha(Console.ReadLine());
+            Console.Write("Entre com o cpf: ");
+            us.setCpf(long.Parse(Console.ReadLine()));
+            us.setAtivo(true);
+
+            pessoas[index] = us;
+
+            return true;
+        }
+
+        public string verUser(int index)
+        {
+            string txt = String.Format("\nNome: {0} , Email: {1} , Senha: {2} , CPF: {3}", pessoas[index].getNome(), pessoas[index].getEmail(), pessoas[index].getSenha(), pessoas[index].getCpf());
+            return txt;
+        }
+
+        public void delUser(int index)
+        {
+            pessoas[index].setAtivo(false);
+
+        }
+
+        public void editUser(int index)
+        {
+            int e;
+            string nnome, nemail, nsenha;
+            long ncpf;
+
+            Console.WriteLine("qual atributo quer editar? [1]nome , [2]email, [3]senha, [4]cpf ");
+            e = int.Parse(Console.ReadLine());
+            if (e == 1)
+            {
+                Console.Write("");
+                nnome = Console.ReadLine();
+                pessoas[index].setNome(nnome);
+            }
+            else if (e == 2)
+            {
+                Console.Write("");
+                nemail = Console.ReadLine();
+                pessoas[index].setEmail(nemail);
+            }
+            else if (e == 3)
+            {
+                Console.Write("");
+                nsenha = Console.ReadLine();
+                pessoas[index].setSenha(nsenha);
+            }
+            else if (e == 4)
+            {
+                Console.Write("");
+                ncpf = (long.Parse(Console.ReadLine()));
+                pessoas[index].setCpf(ncpf);
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Editado");
+            Console.WriteLine(this.verUser(index));
+            Console.WriteLine("---------------");
+
+
+        }
+
+
+
+
+        public void listar()
+        {
+            foreach (var pessoa in pessoas)
+            {
+                if (pessoa.getAtivo())
+                {
+                    string txt = String.Format("\nNome: {0} , Email: {1} , Senha: {2} , CPF: {3}", pessoa.getNome(), pessoa.getEmail(), pessoa.getSenha(), pessoa.getCpf());
+                    Console.WriteLine(txt);
+                }
+            }
+        }
+    }
+}
