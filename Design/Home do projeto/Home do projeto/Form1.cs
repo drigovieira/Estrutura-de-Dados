69881abff5eb77dde63e07cf742cb51ee6b8ff0c
Mateus Silva <lmateussilval@hotmail.com>
2019-10-08 16:57:20 -0300

    +Home_do_projeto

---
+++ Design/Home do projeto/Home do projeto/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home_do_projeto
+{
+    public partial class FormNav : Form
+    {
+        public FormNav()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CardsModulos1_Load(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            FormHome fh = new FormHome();
+            fh.Show();
+            
+        }
+    }
+}
