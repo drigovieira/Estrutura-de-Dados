87c2333c69532f002c38686ffc3538da4248c184
Mateus Silva <48017284+Mteussilva@users.noreply.github.com>
2019-10-12 17:16:12 -0300

    +Organizacao_pastas

--- Design/Pont_Finder/Pont_Finder/formCriarConta.Designer.cs
+++ Design/Pont_Finder/Pont_Finder/formCriarConta.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // formCriarConta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 487);
+            this.Name = "formCriarConta";
             this.Text = "formCriarConta";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Design/Pont_Finder/Pont_Finder/formCriarConta.cs
+++ Design/Pont_Finder/Pont_Finder/formCriarConta.cs
@@ -16,5 +16,10 @@ namespace Pont_Finder
         {
             InitializeComponent();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
