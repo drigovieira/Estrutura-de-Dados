2112e129efdd98e3804d129b0c6445f90f877e5c
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-10-14 15:09:40 -0300

    clones

---
+++ Pont_Finder/Pont_Finder/formLogin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button_CriarC_Click(object sender, EventArgs e)
+        {
+            formCriarConta fCC = new formCriarConta();
+            this.Close();
+            fCC.ShowDialog();
+            
+
+        }
+
+        private void buttonCriarC_Click(object sender, EventArgs e)
+        {
+            formCriarConta fCC = new formCriarConta();
+            fCC.ShowDialog();
+            this.Dispose();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string Login_email = textB_email.Text;
+            string Login_senha = textB_senha.Text;
+        }
+    }
+}
