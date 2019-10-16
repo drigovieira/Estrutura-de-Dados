494b53e3c289adc4ee39caffb733ffcf6d7b55db
@rafaelferreira1951 <rafaelferreira.1951@gmail.com>
2019-10-16 14:49:01 -0300

    +cornavbar

---
+++ Design/teste/Pont_Finder/Pont_Finder/FormPrincipal.cs
@@ -0,0 +1,103 @@
+﻿using Pont_Finder.servicos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder
+{
+    public partial class FormPrincipal : Form
+    {
+        
+        static Color barra = Color.FromArgb(64,64,64);
+        static Panel navbar;
+        static Panel painel;
+
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+            panel1.BackColor = barra;
+            navbar = panel1;
+            painel = panelHome;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formHome fh = new formHome();
+            fh.TopLevel = false;
+            panelHome.Controls.Add(fh);
+            fh.Show();
+        }
+
+        public void panelHome_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        //public static void servicos()
+        //{
+        //    navbar.BackColor = Color.Blue;
+        //}
+
+        public static void Mudar(String x = "")
+        {
+            painel.Controls.Clear();
+            switch (x)
+            {
+                case "Alimentos":
+                    navbar.BackColor = Color.Red;
+                    break;
+                case "Serviços":
+                    formservicos form = new formservicos
+                    {
+                        TopLevel = false
+                    };
+                    painel.Controls.Add(form);
+                    form.Show();
+                    navbar.BackColor = Color.Blue;
+
+                    break;
+                case "Hospedagem":
+                    navbar.BackColor = Color.LimeGreen;
+                    break;
+                case "Eventos":
+                    navbar.BackColor = Color.Blue;
+                    break;
+                case "Avalie":
+                    navbar.BackColor = Color.Blue;
+                    break;
+                default:
+                    formHome fh = new formHome
+                    {
+                        TopLevel = false
+                    };
+                    painel.Controls.Add(fh);
+                    fh.Show();
+                    navbar.BackColor = barra;
+                    break;
+
+            }
+        }
+
+        public static void padrao()
+        {
+            navbar.BackColor = barra;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Mudar();
+        }
+    }
+}
---
+++ Design/teste/Pont_Finder/Pont_Finder/servicos/servicos/servicos.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace servicos
+{
+    partial class servicos
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(servicos));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelservicos = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(92, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1349, 45);
+            this.panel1.TabIndex = 4;
+            // 
+            // panelservicos
+            // 
+            this.panelservicos.Location = new System.Drawing.Point(42, 45);
+            this.panelservicos.Name = "panelservicos";
+            this.panelservicos.Size = new System.Drawing.Size(1265, 675);
+            this.panelservicos.TabIndex = 5;
+            this.panelservicos.Paint += new System.Windows.Forms.PaintEventHandler(this.panelHome_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(1206, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(1242, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(15, 15);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // servicos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 720);
+            this.Controls.Add(this.panelservicos);
+            this.Controls.Add(this.panel1);
+            this.Name = "servicos";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelservicos;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
