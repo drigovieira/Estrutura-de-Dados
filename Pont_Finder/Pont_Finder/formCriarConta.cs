aa35fd6bda424fc43aa11eb04f09e0c632a8de0c
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-10-16 17:55:39 -0300

    validacao email e cpf

--- Pont_Finder/Pont_Finder/formCriarConta.cs
+++ Pont_Finder/Pont_Finder/formCriarConta.cs
@@ -28,22 +28,62 @@ namespace Pont_Finder
             long cad_CPF = long.Parse(textB_cpf.Text);
             string cad_email = textB_email.Text;
             string cad_senha = textB_senha.Text;
+            bool vCpf = true;
+            bool vEmail = true;
 
-            User user = new User();
-            user.Nome = cad_Name;
-            user.Email = cad_email;
-            user.Senha = cad_senha;
-            user.Cpf = cad_CPF;
-            user.Ativo = true;
-            UserList.userAdd(user);
-            
-            MessageBox.Show("Usuário Cadastrado com sucesso!");
 
+
+            foreach (var item in UserList.selectAll())
+            {
+                if (item.Cpf == cad_CPF)
+                    vCpf = false;
+                
+                if (item.Email == cad_email)
+                    vEmail = false;     
+            }
+
+            if (vCpf)
+            {
+                if (vEmail)
+                {
+                    if (Validation.Cpf(cad_CPF))
+                    {
+                        if (Validation.Email(cad_email))
+                        {
+                            User user = new User();
+                            user.Nome = cad_Name;
+                            user.Email = cad_email;
+                            user.Senha = cad_senha;
+                            user.Cpf = cad_CPF;
+                            user.Ativo = true;
+                            UserList.userAdd(user);
+                            MessageBox.Show("Usuário Cadastrado com sucesso!");
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Email inválido");
+                        }
+                      
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cpf inválido");
+                    }
+
+                  
+                }
+                else
+                {
+                    MessageBox.Show("Email já cadastrado no sistema!");
+                }        
+            }
+            else
+            {
+                MessageBox.Show("Cpf já cadastrado no sistema!");
+            }
             //this.Dispose();
-            this.Close();
-            
-
-
+                       
         }
     }
 }
