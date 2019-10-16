c92b4a2c8798fb0a6630e22a36fa0d7f176ac690
Mateus Silva <48017284+Mteussilva@users.noreply.github.com>
2019-10-16 16:37:58 -0300

    +cadEmpresa

---
+++ Design/Modulos/Pont_Finder/Pont_Finder/FormPrincipal.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            formHome fh = new formHome();
+            fh.TopLevel = false;
+            panelHome.Controls.Add(fh);
+            fh.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            formCriarConta fCC = new formCriarConta();
+            fCC.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            formLogin fL = new formLogin();
+            fL.ShowDialog();
+            if (Session.Id != -1)
+            {
+                button4.Visible = false;
+                button1.Visible = false;
+                label1.Text = Session.Nome;
+            }
+        }
+
+        private void Button_salvar_Click(object sender, EventArgs e)
+        {
+            XmlLi xmlli = new XmlLi();
+            //final do programa salvando...
+            xmlli.Drop();
+            int cont = 0;
+            foreach (var item in UserList.selectAll())
+            {
+                xmlli.Add(cont, item.Nome, item.Email, item.Senha, item.Cpf, item.Ativo);
+                cont++;
+            }
+
+            MessageBox.Show("Dados Salvos");
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cadEmpresa ce = new cadEmpresa();
+            ce.Show();
+        }
+    }
+}
---
+++ Design/Modulos/Pont_Finder/Pont_Finder/cadEmpresa.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Pont_Finder
+{
+    partial class cadEmpresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel9);
+            this.panel1.Controls.Add(this.panel10);
+            this.panel1.Controls.Add(this.panel11);
+            this.panel1.Controls.Add(this.panel12);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel13);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1265, 200);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Dubai", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(323, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome da empresa";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 280);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(323, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 36);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome fantasia";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(329, 352);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(323, 535);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 36);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(329, 574);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(329, 501);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(323, 462);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 36);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Endereço";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(329, 426);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(323, 387);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 36);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CNPJ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(328, 723);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(323, 684);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 36);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "CEP";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alimentos",
+            "Serviços",
+            "Hospedagem",
+            "Eventos",
+            "Avalie"});
+            this.comboBox1.Location = new System.Drawing.Point(328, 783);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(328, 651);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label7.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(323, 612);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 36);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Telefone";
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(1169, 51);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(100, 630);
+            this.panel7.TabIndex = 29;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(1064, 51);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(100, 630);
+            this.panel8.TabIndex = 28;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(959, 51);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(100, 630);
+            this.panel9.TabIndex = 27;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(854, 51);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(100, 630);
+            this.panel10.TabIndex = 26;
+            // 
+            // panel11
+            // 
+            this.panel11.Location = new System.Drawing.Point(749, 51);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(100, 630);
+            this.panel11.TabIndex = 25;
+            // 
+            // panel12
+            // 
+            this.panel12.Location = new System.Drawing.Point(644, 51);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(100, 630);
+            this.panel12.TabIndex = 24;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(539, 51);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(100, 630);
+            this.panel6.TabIndex = 23;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(434, 51);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(100, 630);
+            this.panel5.TabIndex = 22;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(329, 51);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(100, 630);
+            this.panel4.TabIndex = 21;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(224, 51);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 630);
+            this.panel3.TabIndex = 20;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(119, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 630);
+            this.panel2.TabIndex = 19;
+            // 
+            // panel13
+            // 
+            this.panel13.Location = new System.Drawing.Point(14, 51);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(100, 630);
+            this.panel13.TabIndex = 18;
+            // 
+            // panel14
+            // 
+            this.panel14.Location = new System.Drawing.Point(1012, 484);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(166, 332);
+            this.panel14.TabIndex = 16;
+            // 
+            // cadEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1282, 665);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "cadEmpresa";
+            this.Text = "cadEmpresa";
+            this.Load += new System.EventHandler(this.cadEmpresa_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+    }
+}
