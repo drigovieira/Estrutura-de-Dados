22802aac59d516339f88e93174697ad10f947061
Mateus Silva <48017284+Mteussilva@users.noreply.github.com>
2019-10-17 16:59:21 -0300

    +bugfix

---
+++ Design/Modulos/Hospedagem/Hospedagem/Hospedagem/Detalhes empresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospedagem
+{
+    public partial class Detalhes_empresa : Form
+    {
+        public Detalhes_empresa()
+        {
+            InitializeComponent();
+        }
+    }
+}
