82310de04dc360f791767506ec3affa672940a4a
Mateus Silva <48017284+Mteussilva@users.noreply.github.com>
2019-10-27 11:23:52 -0300

    +Grid att

--- Design/Modulos/Padrão_Modulos/Projetos/Grid/Grid/Grid conteudo.Designer.cs
+++ Design/Modulos/Padrão_Modulos/Projetos/Grid/Grid/Grid conteudo.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
             this.panel1.Location = new System.Drawing.Point(110, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1055, 651);
