9ad66f7c0c6e6ff04822aee734513760d67775b2
Charles Vinicius <54713741+CharlesVinicius@users.noreply.github.com>
2019-10-29 22:49:36 -0300

    +RecuperarConta

--- Design/Modulos/Hospedagem/EditarQuarto/Form1.Designer.cs
+++ Design/Modulos/Hospedagem/EditarQuarto/Form1.Designer.cs
@@ -73,6 +73,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Dubai", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(799, 16);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 28);
