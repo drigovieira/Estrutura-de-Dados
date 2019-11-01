6f215d4ad3478efeab5c9a92e5c0bf673499751a
Hiero Salim <hierosalim12@gmail.com>
2019-11-01 09:03:41 -0300

    implementação de funcionalidades teste Alimentação

---
+++ Pont_Finder/Pont_Finder/alimentos/ExibirEmpresa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder.alimentos
+{
+    public partial class ExibirEmpresa : UserControl
+    {
+        private string nomeEmpresa;
+        private int ID;
+        public ExibirEmpresa(string nomeComp, int idComp)
+        {
+            nomeEmpresa = nomeComp;
+            ID = idComp;
+            InitializeComponent();
+        }
+
+        private void Panel5_Paint(object sender, PaintEventArgs e)
+        {
+            NomeEmpresa.Text = nomeEmpresa;
+        }
+    }
+}
--- Pont_Finder/Pont_Finder/alimentos/formAlimentos.Designer.cs
+++ Pont_Finder/Pont_Finder/alimentos/formAlimentos.Designer.cs
@@ -34,32 +34,20 @@
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Pesquisar = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PesquisaRestaurante = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.panel8 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel9 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.Top5 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.ListarEmpresas = new System.Windows.Forms.Panel();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            this.panel7.SuspendLayout();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -95,9 +83,9 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.panel2.Controls.Add(this.comboBox1);
-            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.Pesquisar);
             this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.PesquisaRestaurante);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(11, 328);
@@ -112,20 +100,22 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(172, 21);
             this.comboBox1.TabIndex = 11;
+            this.comboBox1.Visible = false;
             // 
-            // button3
+            // Pesquisar
             // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button3.Location = new System.Drawing.Point(200, 158);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 28);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Pesquisar";
-            this.button3.UseVisualStyleBackColor = false;
+            this.Pesquisar.BackColor = System.Drawing.Color.Transparent;
+            this.Pesquisar.FlatAppearance.BorderSize = 0;
+            this.Pesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pesquisar.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pesquisar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Pesquisar.Location = new System.Drawing.Point(200, 85);
+            this.Pesquisar.Name = "Pesquisar";
+            this.Pesquisar.Size = new System.Drawing.Size(103, 28);
+            this.Pesquisar.TabIndex = 9;
+            this.Pesquisar.Text = "Pesquisar";
+            this.Pesquisar.UseVisualStyleBackColor = false;
+            this.Pesquisar.Click += new System.EventHandler(this.Pesquisar_Click);
             // 
             // label4
             // 
@@ -136,13 +126,14 @@
             this.label4.Size = new System.Drawing.Size(70, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Categoria";
+            this.label4.Visible = false;
             // 
-            // textBox1
+            // PesquisaRestaurante
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 90);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(172, 20);
-            this.textBox1.TabIndex = 2;
+            this.PesquisaRestaurante.Location = new System.Drawing.Point(22, 90);
+            this.PesquisaRestaurante.Name = "PesquisaRestaurante";
+            this.PesquisaRestaurante.Size = new System.Drawing.Size(172, 20);
+            this.PesquisaRestaurante.TabIndex = 2;
             // 
             // label3
             // 
@@ -150,9 +141,9 @@
             this.label3.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(17, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.Size = new System.Drawing.Size(52, 25);
             this.label3.TabIndex = 1;
-            this.label3.Text = "Nome";
+            this.label3.Text = "Nome:";
             // 
             // label1
             // 
@@ -173,166 +164,34 @@
             this.panel1.Size = new System.Drawing.Size(0, 0);
             this.panel1.TabIndex = 12;
             // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.Green;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(210, 225);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(82, 28);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Visualizar";
-            this.button6.UseVisualStyleBackColor = false;
-            // 
-            // panel8
-            // 
-            this.panel8.BackColor = System.Drawing.Color.White;
-            this.panel8.Location = new System.Drawing.Point(22, 75);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(270, 136);
-            this.panel8.TabIndex = 6;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Dubai", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(18, 14);
+            this.label7.Location = new System.Drawing.Point(18, 4);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(255, 27);
             this.label7.TabIndex = 5;
             this.label7.Text = "Top 5 Restaurantes Melhor Avaliados";
             // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.panel5.Controls.Add(this.trackBar1);
-            this.panel5.Controls.Add(this.button7);
-            this.panel5.Controls.Add(this.button4);
-            this.panel5.Controls.Add(this.button2);
-            this.panel5.Controls.Add(this.panel7);
-            this.panel5.Controls.Add(this.panel6);
-            this.panel5.Location = new System.Drawing.Point(325, 394);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(625, 200);
-            this.panel5.TabIndex = 16;
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(207, 152);
-            this.trackBar1.Maximum = 5;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(151, 45);
-            this.trackBar1.TabIndex = 11;
-            // 
-            // button7
-            // 
-            this.button7.BackColor = System.Drawing.Color.MidnightBlue;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(364, 158);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(82, 28);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "Avalie";
-            this.button7.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button4.Location = new System.Drawing.Point(8, 158);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(103, 28);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Comentários";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(530, 158);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 28);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Reservar";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.White;
-            this.panel7.Controls.Add(this.button5);
-            this.panel7.Controls.Add(this.button1);
-            this.panel7.Location = new System.Drawing.Point(204, 8);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(411, 120);
-            this.panel7.TabIndex = 6;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.Transparent;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button5.Location = new System.Drawing.Point(3, 89);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(103, 28);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "Localização";
-            this.button5.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Green;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Dubai", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(326, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Visualizar";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.Color.White;
-            this.panel6.Location = new System.Drawing.Point(8, 8);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(190, 120);
-            this.panel6.TabIndex = 5;
-            // 
-            // panel9
-            // 
-            this.panel9.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel9.Location = new System.Drawing.Point(0, 595);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(1265, 100);
-            this.panel9.TabIndex = 17;
-            // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.panel4.Controls.Add(this.button6);
-            this.panel4.Controls.Add(this.panel8);
+            this.panel4.Controls.Add(this.Top5);
             this.panel4.Controls.Add(this.label7);
             this.panel4.Location = new System.Drawing.Point(955, 328);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(310, 267);
+            this.panel4.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
+            this.panel4.Size = new System.Drawing.Size(310, 324);
             this.panel4.TabIndex = 15;
+            // 
+            // Top5
+            // 
+            this.Top5.BackColor = System.Drawing.Color.White;
+            this.Top5.Location = new System.Drawing.Point(22, 34);
+            this.Top5.Name = "Top5";
+            this.Top5.Size = new System.Drawing.Size(270, 270);
+            this.Top5.TabIndex = 6;
             // 
             // label8
             // 
@@ -344,6 +203,22 @@
             this.label8.TabIndex = 19;
             this.label8.Text = "Confira o que temos para você!";
             // 
+            // panel9
+            // 
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel9.Location = new System.Drawing.Point(0, 652);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(1265, 100);
+            this.panel9.TabIndex = 17;
+            // 
+            // ListarEmpresas
+            // 
+            this.ListarEmpresas.AutoSize = true;
+            this.ListarEmpresas.Location = new System.Drawing.Point(325, 393);
+            this.ListarEmpresas.Name = "ListarEmpresas";
+            this.ListarEmpresas.Size = new System.Drawing.Size(624, 196);
+            this.ListarEmpresas.TabIndex = 20;
+            // 
             // formAlimentos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -351,12 +226,12 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1282, 675);
+            this.Controls.Add(this.ListarEmpresas);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.panel10);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel9);
             this.Controls.Add(this.panel4);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -366,10 +241,6 @@
             this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            this.panel7.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.ResumeLayout(false);
@@ -384,26 +255,17 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Pesquisar;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox PesquisaRestaurante;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel9;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel ListarEmpresas;
+        private System.Windows.Forms.Panel Top5;
     }
 }
