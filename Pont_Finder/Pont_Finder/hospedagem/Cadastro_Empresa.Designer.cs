6eafb831abf15872ff80e2495057e30bfce623bb
Miguel <oliveiramiguel142@gmail.com>
2019-11-04 17:06:16 -0300

    eventos - bugfix

--- Pont_Finder/Pont_Finder/hospedagem/Cadastro_Empresa.Designer.cs
+++ Pont_Finder/Pont_Finder/hospedagem/Cadastro_Empresa.Designer.cs
@@ -372,7 +372,7 @@
             this.label1.Font = new System.Drawing.Font("Dubai", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(168, 587);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(286, 33);
+            this.label1.Size = new System.Drawing.Size(286, 32);
             this.label1.TabIndex = 18;
             this.label1.Text = "Selecione a quantidade de estrelas";
             // 
@@ -593,7 +593,7 @@
             this.label17.Font = new System.Drawing.Font("Dubai", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label17.Location = new System.Drawing.Point(175, 474);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(272, 33);
+            this.label17.Size = new System.Drawing.Size(272, 32);
             this.label17.TabIndex = 16;
             this.label17.Text = "Selecione o tipo de Hospedagem";
             // 
@@ -614,7 +614,7 @@
             this.radioButton2.Font = new System.Drawing.Font("Dubai", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton2.Location = new System.Drawing.Point(141, 10);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(73, 25);
+            this.radioButton2.Size = new System.Drawing.Size(73, 26);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Pousada";
@@ -626,7 +626,7 @@
             this.radioButton1.Font = new System.Drawing.Font("Dubai", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton1.Location = new System.Drawing.Point(24, 10);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 25);
+            this.radioButton1.Size = new System.Drawing.Size(58, 26);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Hotel";
@@ -714,7 +714,7 @@
             this.label3.Font = new System.Drawing.Font("Dubai", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(158, 747);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(274, 33);
+            this.label3.Size = new System.Drawing.Size(274, 32);
             this.label3.TabIndex = 4;
             this.label3.Text = "De uma breve descrição do Hotel";
             // 
