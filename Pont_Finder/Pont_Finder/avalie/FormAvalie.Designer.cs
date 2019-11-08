39ddabfcb904ee3008dd10561cc6a26973d85b93
RAPHAELACACIO <48342329+RAPHAELACACIO@users.noreply.github.com>
2019-11-08 17:38:21 -0300

    Update FormAvalie.Designer.cs
    g

--- Pont_Finder/Pont_Finder/avalie/FormAvalie.Designer.cs
+++ Pont_Finder/Pont_Finder/avalie/FormAvalie.Designer.cs
@@ -39,21 +39,28 @@
             this.button3 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label15 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.panel3 = new System.Windows.Forms.Panel();
-            this.nomepanelfeed = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Top5 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_Pesquisar = new System.Windows.Forms.Button();
             this.panel10.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel5.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.nomepanelfeed.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -105,14 +112,14 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(67, 316);
+            this.button1.Location = new System.Drawing.Point(314, 203);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 52);
+            this.button1.Size = new System.Drawing.Size(630, 43);
             this.button1.TabIndex = 13;
-            this.button1.Text = "Atualizar Feed";
+            this.button1.Text = "Atualize as ultimas postagens !";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -121,10 +128,10 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.Highlight;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.Location = new System.Drawing.Point(67, 237);
+            this.button3.Location = new System.Drawing.Point(145, 215);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(158, 52);
             this.button3.TabIndex = 12;
@@ -147,7 +154,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel7);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.richTextBox1);
             this.panel1.Location = new System.Drawing.Point(1, 203);
@@ -156,54 +163,14 @@
             this.panel1.TabIndex = 23;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.White;
-            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel5.Controls.Add(this.label15);
-            this.panel5.Location = new System.Drawing.Point(0, 170);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(310, 325);
-            this.panel5.TabIndex = 25;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(84, 145);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(160, 20);
-            this.label15.TabIndex = 1;
-            this.label15.Text = "Propaganda modulos";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(84, 71);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(155, 20);
-            this.label13.TabIndex = 0;
-            this.label13.Text = "Propaganda Externa";
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel4.Controls.Add(this.label13);
-            this.panel4.Location = new System.Drawing.Point(0, 5);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(310, 160);
-            this.panel4.TabIndex = 19;
-            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel6);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Location = new System.Drawing.Point(952, 203);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(310, 675);
+            this.panel2.Size = new System.Drawing.Size(310, 1188);
             this.panel2.TabIndex = 24;
             // 
             // contextMenuStrip2
@@ -216,36 +183,145 @@
             this.panel3.AutoScroll = true;
             this.panel3.Location = new System.Drawing.Point(316, 246);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(627, 500);
+            this.panel3.Size = new System.Drawing.Size(627, 1145);
             this.panel3.TabIndex = 25;
             // 
-            // nomepanelfeed
+            // panel4
             // 
-            this.nomepanelfeed.BackColor = System.Drawing.SystemColors.Highlight;
-            this.nomepanelfeed.Controls.Add(this.label1);
-            this.nomepanelfeed.Location = new System.Drawing.Point(316, 203);
-            this.nomepanelfeed.Name = "nomepanelfeed";
-            this.nomepanelfeed.Size = new System.Drawing.Size(626, 43);
-            this.nomepanelfeed.TabIndex = 26;
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label15);
+            this.panel4.Location = new System.Drawing.Point(0, 529);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(310, 325);
+            this.panel4.TabIndex = 39;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(84, 145);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(160, 20);
+            this.label15.TabIndex = 1;
+            this.label15.Text = "Propaganda modulos";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label13);
+            this.panel5.Location = new System.Drawing.Point(952, 560);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(310, 160);
+            this.panel5.TabIndex = 40;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(84, 71);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(155, 20);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Propaganda Externa";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel6.Controls.Add(this.Top5);
+            this.panel6.Controls.Add(this.label7);
+            this.panel6.Location = new System.Drawing.Point(0, 1);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(310, 343);
+            this.panel6.TabIndex = 41;
+            // 
+            // Top5
+            // 
+            this.Top5.BackColor = System.Drawing.Color.White;
+            this.Top5.Location = new System.Drawing.Point(22, 41);
+            this.Top5.Name = "Top5";
+            this.Top5.Size = new System.Drawing.Size(270, 286);
+            this.Top5.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(48, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(226, 24);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "5 maiores reclamações";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel7.Controls.Add(this.button_Pesquisar);
+            this.panel7.Controls.Add(this.comboBox1);
+            this.panel7.Controls.Add(this.label1);
+            this.panel7.Controls.Add(this.label2);
+            this.panel7.Location = new System.Drawing.Point(1, 295);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(308, 195);
+            this.panel7.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(243, 6);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ultimas Postagens";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Categoria";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Pesquisar Reclamações";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Infraestrutura",
+            "Saúde",
+            "Segurança"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(295, 28);
+            this.comboBox1.TabIndex = 78;
+            // 
+            // button_Pesquisar
+            // 
+            this.button_Pesquisar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(95)))), ((int)(((byte)(233)))));
+            this.button_Pesquisar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(106)))), ((int)(((byte)(255)))));
+            this.button_Pesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Pesquisar.ForeColor = System.Drawing.Color.White;
+            this.button_Pesquisar.Location = new System.Drawing.Point(192, 127);
+            this.button_Pesquisar.Name = "button_Pesquisar";
+            this.button_Pesquisar.Size = new System.Drawing.Size(110, 34);
+            this.button_Pesquisar.TabIndex = 79;
+            this.button_Pesquisar.Text = "Buscar";
+            this.button_Pesquisar.UseVisualStyleBackColor = false;
             // 
             // FormAvalie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1282, 675);
-            this.Controls.Add(this.nomepanelfeed);
+            this.ClientSize = new System.Drawing.Size(1282, 767);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel8);
             this.Controls.Add(this.panel10);
             this.Controls.Add(this.panel11);
@@ -258,13 +334,15 @@
             this.panel10.ResumeLayout(false);
             this.panel10.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.nomepanelfeed.ResumeLayout(false);
-            this.nomepanelfeed.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -278,15 +356,21 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel nomepanelfeed;
-        private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel Top5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button_Pesquisar;
     }
 }
