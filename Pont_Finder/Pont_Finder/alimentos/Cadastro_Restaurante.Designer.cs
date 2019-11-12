af6506079fc57732f816522e17a2f383ef3666f9
Hiero Salim <hierosalim12@gmail.com>
2019-11-11 23:39:56 -0300

    Inserções Alimentos

--- Pont_Finder/Pont_Finder/alimentos/Cadastro_Restaurante.Designer.cs
+++ Pont_Finder/Pont_Finder/alimentos/Cadastro_Restaurante.Designer.cs
@@ -31,21 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastro_Restaurante));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel11 = new System.Windows.Forms.Panel();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.tbNum = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbRua = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbTel = new System.Windows.Forms.TextBox();
+            this.tbFantasia = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tbBairro = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.nomeempresa = new System.Windows.Forms.TextBox();
+            this.tbCep = new System.Windows.Forms.TextBox();
+            this.tbNomeEmpresa = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
@@ -69,7 +69,7 @@
             this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnCad = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.checkBox9 = new System.Windows.Forms.CheckBox();
@@ -127,42 +127,42 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(5, 301);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(310, 691);
+            this.panel1.Size = new System.Drawing.Size(310, 562);
             this.panel1.TabIndex = 51;
             // 
             // panel11
             // 
             this.panel11.BackColor = System.Drawing.Color.White;
             this.panel11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel11.Controls.Add(this.textBox8);
+            this.panel11.Controls.Add(this.tbNum);
             this.panel11.Controls.Add(this.label8);
             this.panel11.Controls.Add(this.label1);
-            this.panel11.Controls.Add(this.textBox3);
-            this.panel11.Controls.Add(this.textBox5);
+            this.panel11.Controls.Add(this.tbEmail);
+            this.panel11.Controls.Add(this.tbRua);
             this.panel11.Controls.Add(this.label5);
-            this.panel11.Controls.Add(this.textBox7);
-            this.panel11.Controls.Add(this.textBox2);
+            this.panel11.Controls.Add(this.tbTel);
+            this.panel11.Controls.Add(this.tbFantasia);
             this.panel11.Controls.Add(this.label6);
-            this.panel11.Controls.Add(this.textBox4);
+            this.panel11.Controls.Add(this.tbBairro);
             this.panel11.Controls.Add(this.label7);
             this.panel11.Controls.Add(this.label9);
             this.panel11.Controls.Add(this.label11);
-            this.panel11.Controls.Add(this.textBox6);
-            this.panel11.Controls.Add(this.nomeempresa);
+            this.panel11.Controls.Add(this.tbCep);
+            this.panel11.Controls.Add(this.tbNomeEmpresa);
             this.panel11.Controls.Add(this.label12);
             this.panel11.Controls.Add(this.label13);
             this.panel11.Location = new System.Drawing.Point(320, 301);
             this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(625, 691);
+            this.panel11.Size = new System.Drawing.Size(625, 562);
             this.panel11.TabIndex = 57;
             // 
-            // textBox8
+            // tbNum
             // 
-            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox8.Location = new System.Drawing.Point(100, 307);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(170, 22);
-            this.textBox8.TabIndex = 22;
+            this.tbNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNum.Location = new System.Drawing.Point(100, 307);
+            this.tbNum.Name = "tbNum";
+            this.tbNum.Size = new System.Drawing.Size(170, 22);
+            this.tbNum.TabIndex = 22;
             // 
             // label8
             // 
@@ -178,27 +178,27 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(100, 349);
+            this.label1.Location = new System.Drawing.Point(100, 358);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 20);
             this.label1.TabIndex = 20;
             this.label1.Text = "Bairro";
             // 
-            // textBox3
+            // tbEmail
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(100, 534);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(415, 22);
-            this.textBox3.TabIndex = 6;
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(100, 471);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(415, 22);
+            this.tbEmail.TabIndex = 6;
             // 
-            // textBox5
+            // tbRua
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(100, 231);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(415, 22);
-            this.textBox5.TabIndex = 10;
+            this.tbRua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRua.Location = new System.Drawing.Point(100, 231);
+            this.tbRua.Name = "tbRua";
+            this.tbRua.Size = new System.Drawing.Size(415, 22);
+            this.tbRua.TabIndex = 10;
             // 
             // label5
             // 
@@ -210,46 +210,46 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "Rua";
             // 
-            // textBox7
+            // tbTel
             // 
-            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(345, 461);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(170, 22);
-            this.textBox7.TabIndex = 15;
+            this.tbTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTel.Location = new System.Drawing.Point(345, 388);
+            this.tbTel.Name = "tbTel";
+            this.tbTel.Size = new System.Drawing.Size(170, 22);
+            this.tbTel.TabIndex = 15;
             // 
-            // textBox2
+            // tbFantasia
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(100, 123);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(415, 22);
-            this.textBox2.TabIndex = 4;
+            this.tbFantasia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFantasia.Location = new System.Drawing.Point(100, 123);
+            this.tbFantasia.Name = "tbFantasia";
+            this.tbFantasia.Size = new System.Drawing.Size(415, 22);
+            this.tbFantasia.TabIndex = 4;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(100, 505);
+            this.label6.Location = new System.Drawing.Point(100, 442);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Email\r\n";
             // 
-            // textBox4
+            // tbBairro
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(100, 388);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(415, 22);
-            this.textBox4.TabIndex = 8;
+            this.tbBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBairro.Location = new System.Drawing.Point(100, 388);
+            this.tbBairro.Name = "tbBairro";
+            this.tbBairro.Size = new System.Drawing.Size(172, 22);
+            this.tbBairro.TabIndex = 8;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(345, 431);
+            this.label7.Location = new System.Drawing.Point(345, 358);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(79, 20);
             this.label7.TabIndex = 14;
@@ -276,21 +276,21 @@
             this.label11.TabIndex = 9;
             this.label11.Text = "Endereço:";
             // 
-            // textBox6
+            // tbCep
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(100, 461);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(170, 22);
-            this.textBox6.TabIndex = 12;
+            this.tbCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCep.Location = new System.Drawing.Point(345, 307);
+            this.tbCep.Name = "tbCep";
+            this.tbCep.Size = new System.Drawing.Size(170, 22);
+            this.tbCep.TabIndex = 12;
             // 
-            // nomeempresa
+            // tbNomeEmpresa
             // 
-            this.nomeempresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nomeempresa.Location = new System.Drawing.Point(100, 48);
-            this.nomeempresa.Name = "nomeempresa";
-            this.nomeempresa.Size = new System.Drawing.Size(415, 22);
-            this.nomeempresa.TabIndex = 2;
+            this.tbNomeEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNomeEmpresa.Location = new System.Drawing.Point(100, 48);
+            this.tbNomeEmpresa.Name = "tbNomeEmpresa";
+            this.tbNomeEmpresa.Size = new System.Drawing.Size(415, 22);
+            this.tbNomeEmpresa.TabIndex = 2;
             // 
             // label12
             // 
@@ -306,7 +306,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(100, 431);
+            this.label13.Location = new System.Drawing.Point(345, 277);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(44, 20);
             this.label13.TabIndex = 11;
@@ -330,7 +330,7 @@
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Location = new System.Drawing.Point(950, 301);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(310, 691);
+            this.panel2.Size = new System.Drawing.Size(310, 562);
             this.panel2.TabIndex = 58;
             // 
             // panel3
@@ -355,7 +355,7 @@
             this.panel3.Controls.Add(this.radioButton5);
             this.panel3.Controls.Add(this.radioButton4);
             this.panel3.Controls.Add(this.radioButton1);
-            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.btnCad);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.panel6);
             this.panel3.Controls.Add(this.textBox1);
@@ -541,18 +541,19 @@
             this.radioButton1.Text = "Segunda - Feira";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnCad
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(95)))), ((int)(((byte)(233)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(204, 1143);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 50);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "Continuar";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnCad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(95)))), ((int)(((byte)(233)))));
+            this.btnCad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCad.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCad.ForeColor = System.Drawing.Color.White;
+            this.btnCad.Location = new System.Drawing.Point(204, 1143);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(220, 50);
+            this.btnCad.TabIndex = 14;
+            this.btnCad.Text = "Continuar";
+            this.btnCad.UseVisualStyleBackColor = false;
+            this.btnCad.Click += new System.EventHandler(this.BtnCad_Click);
             // 
             // label4
             // 
@@ -957,24 +958,24 @@
         private System.Windows.Forms.Panel panel10;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel11;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbRua;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbTel;
+        private System.Windows.Forms.TextBox tbFantasia;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox tbBairro;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox nomeempresa;
+        private System.Windows.Forms.TextBox tbCep;
+        private System.Windows.Forms.TextBox tbNomeEmpresa;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnCad;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.CheckBox checkBox4;
@@ -1005,7 +1006,7 @@
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox tbNum;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox16;
--- Pont_Finder/Pont_Finder/alimentos/Cadastro_Restaurante.cs
+++ Pont_Finder/Pont_Finder/alimentos/Cadastro_Restaurante.cs
@@ -12,6 +12,7 @@ namespace Pont_Finder.alimentos
 {
     public partial class Cadastro_Restaurante : Form
     {
+        CompanyList complist = new CompanyList();
         public Cadastro_Restaurante()
         {
             InitializeComponent();
@@ -25,6 +26,20 @@ namespace Pont_Finder.alimentos
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnCad_Click(object sender, EventArgs e)
+        {
+            Company comp = new Company();
+            comp.Nome = tbNomeEmpresa.Text;
+            comp.NomeFantasia = tbFantasia.Text;
+            comp.Rua = tbRua.Text;
+            comp.Numero = Convert.ToInt32(tbNum.Text);
+            comp.Cep = Convert.ToInt32(tbCep.Text);
+            comp.Bairro = tbBairro.Text;
+            comp.TelComercial = Convert.ToInt32(tbTel.Text);
+            comp.Email = tbEmail.Text;
+            complist.CompAdd(comp);
         }
     }
 }
