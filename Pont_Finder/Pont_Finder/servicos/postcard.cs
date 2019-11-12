9cd4ebb0da1cd3ad2c0ab569b518d4bb29d7f4b3
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-11-12 10:28:30 -0300

    servicos - like easy

--- Pont_Finder/Pont_Finder/servicos/postcard.cs
+++ Pont_Finder/Pont_Finder/servicos/postcard.cs
@@ -13,18 +13,9 @@ namespace Pont_Finder.servicos
 {
     public partial class PostCard : UserControl
     {
-        private int id;
-        private string titulo;
-        private double valor;
-        private string descricao;
-        private bool ativo;
-        private string image;
-        private long cpf;
-        private long cnpj;
-        private DateTime data;
-        private long likes;
-        private List<long[]> listlikes = new List<long[]>();
+        private classes.Post post;
 
+       
         private long like;
         private long deslike;
 
@@ -42,97 +33,53 @@ namespace Pont_Finder.servicos
 
         public PostCard(int id)
         {
+            this.post = classes.PostList.thisSelectId(id);
+
+
             classes.Post post = new classes.Post();
             post = classes.PostList.SelectId(id);
-            this.titulo = post.Titulo;
-            this.valor = post.Valor;
-            this.id = post.Id;
-            this.likes = post.Likes;
-            this.ativo = post.Ativo;
-            this.image = post.Image;
-            this.data = post.Data;
-            this.cpf = post.Cpf;
-            this.cnpj = post.Cnpj;
-            this.descricao = post.Descricao;
+           
 
 
        
             InitializeComponent();
 
 
-            if (post.Image != null)
-                pb_icone.ImageLocation = post.Image;
+            if (this.post.Image != null)
+                pb_icone.ImageLocation = this.post.Image;
             else
                 pb_icone.ImageLocation = "..//..//servicos//data//images//posts//offImage.png";
 
-            this.like = post.Joinha;
-            this.deslike = post.DeJoinha;
+   
 
-            lb_like.Text = this.like + "";
-            lb_deslike.Text = this.deslike + "";
+            lb_like.Text = this.post.Joinha + "";
+            lb_deslike.Text = this.post.DeJoinha + "";
 
-            
+            pb_up.Image = up;
+            pb_down.Image = down;
 
             if (Session.Online)
             {
-                long userlike = post.userLike(Session.Cpf);
+                long userlike = this.post.userLike(Session.Cpf);
+
                 if (userlike == 1)
-                {
-                    pb_up.Image = upv;
-                    pb_down.Image = down;
-                   // lb_like.ForeColor = System.Drawing.Color.Blue;
-                }
+                    pb_up.Image = upv;               
                 else if (userlike == -1)
-                {
-                    pb_up.Image = up;
-                    pb_down.Image = downv;
-                   // lb_like.ForeColor = System.Drawing.Color.Red;
-                }
+                    pb_down.Image = downv;               
             }
-            else
-            {
-                pb_up.Image = up;
-                pb_down.Image = down;
-            }
-
-            lb_data.Text = "Postado em: " + data;
-
-            lb_titulo.Text = this.titulo;
-            lb_descricao.Text = this.descricao;
-            lb_valor.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", this.valor);
-
-
-
-        }
-
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Visualizacoes_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            //botao de likes
-        }
-
-        private void Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-          
-
            
 
+            lb_data.Text = "Postado em: " + this.post.Data;
+
+            lb_titulo.Text = this.post.Titulo;
+            lb_descricao.Text = this.post.Descricao;
+            lb_valor.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", this.post.Valor);
+
+
+
         }
+
+
 
         private void PictureBox1_Click(object sender, EventArgs e)
         {
@@ -144,45 +91,29 @@ namespace Pont_Finder.servicos
 
         }
 
-        private void tiposervico_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void like_Click(object sender, EventArgs e)
-        {
-
-        }
+      
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            //Like
+            // Dar Um Like
             if (Session.Online)
             {
                 if (pb_up.Image == upv)
-                {
-                    classes.Post post = classes.PostList.SelectId(this.id);
-                    int vLike = post.vLike(Session.Cpf);
-                    post.like(0, Session.Cpf);
-                    lb_like.Text = "" + post.Joinha;
-                    lb_deslike.Text = "" + post.DeJoinha;
+                {                   
+                    post.like(0, Session.Cpf);     
                     pb_up.Image = up;
-                    //lb_like.ForeColor = System.Drawing.Color.Blue;
                     pb_down.Image = down;
                 }
                 else
                 {
-                    classes.Post post = classes.PostList.SelectId(this.id);
-                    int vLike = post.vLike(Session.Cpf);
-                    post.like(1, Session.Cpf);
-                    lb_like.Text = "" + post.Joinha;
-                    lb_deslike.Text = "" + post.DeJoinha;
-                    pb_up.Image = upv;
-                    //lb_like.ForeColor = System.Drawing.Color.Blue;
+                    post.like(1, Session.Cpf);               
+                    pb_up.Image = upv;                 
                     pb_down.Image = down;
                 }
+                lb_like.Text = "" + post.Joinha;
+                lb_deslike.Text = "" + post.DeJoinha;
 
-               
+
             }
             else
             {
@@ -196,29 +127,19 @@ namespace Pont_Finder.servicos
             if (Session.Online)
             {
                 if (pb_down.Image == downv)
-                {
-                    classes.Post post = classes.PostList.SelectId(this.id);
-                    int vLike = post.vLike(Session.Cpf);
+                {                  
                     post.like(0, Session.Cpf);
-                    lb_like.Text = "" + post.Joinha;
-                    lb_deslike.Text = "" + post.DeJoinha;
                     pb_down.Image = down;
-                    //lb_like.ForeColor = System.Drawing.Color.Red;
                     pb_up.Image = up;
                 }
                 else
-                {
-                    classes.Post post = classes.PostList.SelectId(this.id);
-                    int vLike = post.vLike(Session.Cpf);
-                    post.like(-1, Session.Cpf);
-                    lb_like.Text = "" + post.Joinha;
-                    lb_deslike.Text = "" + post.DeJoinha;
+                {                  
+                    post.like(-1, Session.Cpf);              
                     pb_down.Image = downv;
-                    //lb_like.ForeColor = System.Drawing.Color.Red;
                     pb_up.Image = up;
                 }
-
-                
+                lb_like.Text = "" + post.Joinha;
+                lb_deslike.Text = "" + post.DeJoinha;
             }
             else
             {
@@ -226,35 +147,19 @@ namespace Pont_Finder.servicos
             }
         }
 
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void valor_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void visualizacoes_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void descricao_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void Lb_descricao_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Label1_Click_3(object sender, EventArgs e)
-        {
-
-        }
+      
 
         private void Lb_data_Click(object sender, EventArgs e)
         {
@@ -263,21 +168,20 @@ namespace Pont_Finder.servicos
 
         private void bt_obter_Click(object sender, EventArgs e)
         {
-            FormPrincipal.MudarForm("servicos", new FormVisualizarPost(id, new FormServicos()));
+            FormPrincipal.MudarForm("servicos", new FormVisualizarPost(this.post.Id, new FormServicos()));
         }
 
         private void PostCard_Load(object sender, EventArgs e)
         {
-            if (cnpj == -1)
+            if (this.post.Cnpj == -1)
             {
-                User u = UserList.selectCpf(cpf);
+                User u = UserList.selectCpf(this.post.Cpf);
                 pb_user.ImageLocation = u.Image;
                 lb_username.Text = u.Nome;
             }
             else
             {
-
-                classes.Empresa emp = classes.ListaEmpresa.selectCpf(cpf);
+                classes.Empresa emp = classes.ListaEmpresa.selectCpf(this.post.Cpf);
                 pb_user.ImageLocation = emp.Image;
                 lb_username.Text = emp.Nome;
 
