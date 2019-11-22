be64f2aa2895ac9f9495b55ddcd5e773e49f8b76
Mateus Silva <lmteussilva@hotmail.com>
2019-11-22 09:44:06 -0300

    +Serviços Vizualizar serviços ADM

---
+++ Pont_Finder/Pont_Finder/servicos/ServicosADM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder.servicos
+{
+    public partial class ServicosADM : Form
+    {
+        public ServicosADM()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pont_Finder/Pont_Finder/servicos/User_control/CardSev_Agenda.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Pont_Finder.servicos.User_control
+{
+    partial class CardSev_Agenda
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_data = new System.Windows.Forms.Label();
+            this.lb_descricao = new System.Windows.Forms.Label();
+            this.pb_icone = new System.Windows.Forms.PictureBox();
+            this.lb_username = new System.Windows.Forms.Label();
+            this.pb_user = new System.Windows.Forms.PictureBox();
+            this.bt_obter = new System.Windows.Forms.Button();
+            this.lb_numero = new System.Windows.Forms.Label();
+            this.lb_bairro = new System.Windows.Forms.Label();
+            this.lb_cep = new System.Windows.Forms.Label();
+            this.lb_rua = new System.Windows.Forms.Label();
+            this.lb_userEmail = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_icone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_user)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_data
+            // 
+            this.lb_data.AutoSize = true;
+            this.lb_data.BackColor = System.Drawing.Color.Transparent;
+            this.lb_data.Font = new System.Drawing.Font("Dubai", 8F);
+            this.lb_data.Location = new System.Drawing.Point(769, 93);
+            this.lb_data.Margin = new System.Windows.Forms.Padding(5);
+            this.lb_data.Name = "lb_data";
+            this.lb_data.Size = new System.Drawing.Size(100, 18);
+            this.lb_data.TabIndex = 43;
+            this.lb_data.Text = "Agendado para para";
+            this.lb_data.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lb_descricao
+            // 
+            this.lb_descricao.BackColor = System.Drawing.Color.Transparent;
+            this.lb_descricao.Font = new System.Drawing.Font("Dubai", 9F);
+            this.lb_descricao.Location = new System.Drawing.Point(250, 100);
+            this.lb_descricao.Margin = new System.Windows.Forms.Padding(5, 3, 5, 5);
+            this.lb_descricao.MaximumSize = new System.Drawing.Size(500, 65);
+            this.lb_descricao.MinimumSize = new System.Drawing.Size(500, 65);
+            this.lb_descricao.Name = "lb_descricao";
+            this.lb_descricao.Size = new System.Drawing.Size(500, 65);
+            this.lb_descricao.TabIndex = 42;
+            this.lb_descricao.Text = "Descricao:";
+            // 
+            // pb_icone
+            // 
+            this.pb_icone.Location = new System.Drawing.Point(15, 12);
+            this.pb_icone.Margin = new System.Windows.Forms.Padding(15, 15, 5, 15);
+            this.pb_icone.Name = "pb_icone";
+            this.pb_icone.Size = new System.Drawing.Size(200, 150);
+            this.pb_icone.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_icone.TabIndex = 33;
+            this.pb_icone.TabStop = false;
+            // 
+            // lb_username
+            // 
+            this.lb_username.AutoSize = true;
+            this.lb_username.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_username.Location = new System.Drawing.Point(800, 23);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(33, 17);
+            this.lb_username.TabIndex = 39;
+            this.lb_username.Text = "user";
+            // 
+            // pb_user
+            // 
+            this.pb_user.Location = new System.Drawing.Point(743, 15);
+            this.pb_user.Margin = new System.Windows.Forms.Padding(15);
+            this.pb_user.Name = "pb_user";
+            this.pb_user.Size = new System.Drawing.Size(50, 50);
+            this.pb_user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_user.TabIndex = 38;
+            this.pb_user.TabStop = false;
+            // 
+            // bt_obter
+            // 
+            this.bt_obter.BackColor = System.Drawing.Color.RoyalBlue;
+            this.bt_obter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_obter.Font = new System.Drawing.Font("Dubai", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.bt_obter.ForeColor = System.Drawing.Color.White;
+            this.bt_obter.Location = new System.Drawing.Point(868, 133);
+            this.bt_obter.Margin = new System.Windows.Forms.Padding(15);
+            this.bt_obter.Name = "bt_obter";
+            this.bt_obter.Size = new System.Drawing.Size(127, 32);
+            this.bt_obter.TabIndex = 37;
+            this.bt_obter.Text = "Visualizar";
+            this.bt_obter.UseVisualStyleBackColor = false;
+            // 
+            // lb_numero
+            // 
+            this.lb_numero.AutoSize = true;
+            this.lb_numero.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_numero.Location = new System.Drawing.Point(252, 49);
+            this.lb_numero.Name = "lb_numero";
+            this.lb_numero.Size = new System.Drawing.Size(56, 17);
+            this.lb_numero.TabIndex = 47;
+            this.lb_numero.Text = "Número";
+            // 
+            // lb_bairro
+            // 
+            this.lb_bairro.AutoSize = true;
+            this.lb_bairro.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_bairro.Location = new System.Drawing.Point(252, 32);
+            this.lb_bairro.Name = "lb_bairro";
+            this.lb_bairro.Size = new System.Drawing.Size(43, 17);
+            this.lb_bairro.TabIndex = 46;
+            this.lb_bairro.Text = "Bairro";
+            // 
+            // lb_cep
+            // 
+            this.lb_cep.AutoSize = true;
+            this.lb_cep.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_cep.Location = new System.Drawing.Point(251, 66);
+            this.lb_cep.Name = "lb_cep";
+            this.lb_cep.Size = new System.Drawing.Size(30, 17);
+            this.lb_cep.TabIndex = 45;
+            this.lb_cep.Text = "CEP";
+            // 
+            // lb_rua
+            // 
+            this.lb_rua.AutoSize = true;
+            this.lb_rua.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_rua.Location = new System.Drawing.Point(251, 15);
+            this.lb_rua.Name = "lb_rua";
+            this.lb_rua.Size = new System.Drawing.Size(30, 17);
+            this.lb_rua.TabIndex = 44;
+            this.lb_rua.Text = "Rua";
+            // 
+            // lb_userEmail
+            // 
+            this.lb_userEmail.AutoSize = true;
+            this.lb_userEmail.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_userEmail.Location = new System.Drawing.Point(797, 48);
+            this.lb_userEmail.Name = "lb_userEmail";
+            this.lb_userEmail.Size = new System.Drawing.Size(47, 17);
+            this.lb_userEmail.TabIndex = 48;
+            this.lb_userEmail.Text = "E-mail:";
+            // 
+            // CardSev_Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lb_userEmail);
+            this.Controls.Add(this.lb_numero);
+            this.Controls.Add(this.lb_bairro);
+            this.Controls.Add(this.lb_cep);
+            this.Controls.Add(this.lb_rua);
+            this.Controls.Add(this.lb_data);
+            this.Controls.Add(this.lb_descricao);
+            this.Controls.Add(this.pb_icone);
+            this.Controls.Add(this.lb_username);
+            this.Controls.Add(this.pb_user);
+            this.Controls.Add(this.bt_obter);
+            this.Name = "CardSev_Agenda";
+            this.Size = new System.Drawing.Size(1010, 180);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_icone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_user)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lb_data;
+        private System.Windows.Forms.Label lb_descricao;
+        private System.Windows.Forms.PictureBox pb_icone;
+        private System.Windows.Forms.Label lb_username;
+        private System.Windows.Forms.PictureBox pb_user;
+        private System.Windows.Forms.Button bt_obter;
+        private System.Windows.Forms.Label lb_numero;
+        private System.Windows.Forms.Label lb_bairro;
+        private System.Windows.Forms.Label lb_cep;
+        private System.Windows.Forms.Label lb_rua;
+        private System.Windows.Forms.Label lb_userEmail;
+    }
+}
