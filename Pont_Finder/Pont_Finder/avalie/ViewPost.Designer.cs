47ae1519282ad8fdcfcb810195c7dd48fcfd67da
Mateus Silva <lmteussilva@hotmail.com>
2019-11-22 11:53:50 -0300

    +avalie_bugfix

--- Pont_Finder/Pont_Finder/avalie/ViewPost.Designer.cs
+++ Pont_Finder/Pont_Finder/avalie/ViewPost.Designer.cs
@@ -28,17 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewPost));
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
             this.tb_resposta = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel10 = new System.Windows.Forms.Panel();
-            this.label15 = new System.Windows.Forms.Label();
-            this.panel9 = new System.Windows.Forms.Panel();
             this.openIcone = new System.Windows.Forms.OpenFileDialog();
-            this.panel7 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.bt_postar = new System.Windows.Forms.Button();
             this.bt_editar = new System.Windows.Forms.Button();
@@ -55,53 +47,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel8.SuspendLayout();
-            this.panel10.SuspendLayout();
-            this.panel9.SuspendLayout();
-            this.panel7.SuspendLayout();
             this.panel6.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_imagem)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel8
-            // 
-            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
-            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel8.Controls.Add(this.label6);
-            this.panel8.Location = new System.Drawing.Point(2, 0);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(1262, 200);
-            this.panel8.TabIndex = 25;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.label6.Font = new System.Drawing.Font("Dubai", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(485, 98);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(354, 63);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Expresse sua opinião";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(84, 71);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(155, 20);
-            this.label13.TabIndex = 0;
-            this.label13.Text = "Propaganda Externa";
-            // 
             // tb_resposta
             // 
             this.tb_resposta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tb_resposta.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_resposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_resposta.Location = new System.Drawing.Point(28, 342);
             this.tb_resposta.Name = "tb_resposta";
             this.tb_resposta.Size = new System.Drawing.Size(558, 119);
@@ -120,50 +74,9 @@
             this.label1.TabIndex = 66;
             this.label1.Text = "Resposta";
             // 
-            // panel10
-            // 
-            this.panel10.BackColor = System.Drawing.Color.White;
-            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel10.Controls.Add(this.label13);
-            this.panel10.Location = new System.Drawing.Point(0, 5);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(310, 160);
-            this.panel10.TabIndex = 19;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(84, 145);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(160, 20);
-            this.label15.TabIndex = 1;
-            this.label15.Text = "Propaganda modulos";
-            // 
-            // panel9
-            // 
-            this.panel9.BackColor = System.Drawing.Color.White;
-            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel9.Controls.Add(this.label15);
-            this.panel9.Location = new System.Drawing.Point(0, 170);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(310, 325);
-            this.panel9.TabIndex = 25;
-            // 
             // openIcone
             // 
             this.openIcone.FileName = "icone";
-            // 
-            // panel7
-            // 
-            this.panel7.AutoScroll = true;
-            this.panel7.BackColor = System.Drawing.Color.Transparent;
-            this.panel7.Controls.Add(this.panel9);
-            this.panel7.Controls.Add(this.panel10);
-            this.panel7.Location = new System.Drawing.Point(922, 206);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(310, 675);
-            this.panel7.TabIndex = 24;
             // 
             // panel6
             // 
@@ -184,7 +97,7 @@
             this.panel6.Controls.Add(this.tb_resposta);
             this.panel6.Controls.Add(this.label1);
             this.panel6.Controls.Add(this.panel1);
-            this.panel6.Location = new System.Drawing.Point(305, 213);
+            this.panel6.Location = new System.Drawing.Point(305, 62);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(614, 669);
             this.panel6.TabIndex = 23;
@@ -354,36 +267,17 @@
             this.label2.TabIndex = 80;
             this.label2.Text = "Descrição";
             // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Transparent;
-            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel4.Location = new System.Drawing.Point(3, 212);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(298, 668);
-            this.panel4.TabIndex = 22;
-            // 
             // ViewPost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1249, 749);
-            this.Controls.Add(this.panel8);
-            this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel6);
-            this.Controls.Add(this.panel4);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ViewPost";
             this.Text = "ViewPost";
             this.Load += new System.EventHandler(this.ViewPost_Load);
-            this.panel8.ResumeLayout(false);
-            this.panel8.PerformLayout();
-            this.panel10.ResumeLayout(false);
-            this.panel10.PerformLayout();
-            this.panel9.ResumeLayout(false);
-            this.panel9.PerformLayout();
-            this.panel7.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_imagem)).EndInit();
@@ -394,19 +288,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label13;
         private System.Windows.Forms.RichTextBox tb_resposta;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel10;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.Panel panel9;
         private System.Windows.Forms.OpenFileDialog openIcone;
-        private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lb_data;
         private System.Windows.Forms.Label label2;
