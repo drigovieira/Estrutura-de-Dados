95a6b7ad7dfc157172d5cc4085910a68c14161d6
Mateus Silva <lmteussilva@hotmail.com>
2019-11-22 09:44:51 -0300

    +serviços card serviços ADM

--- Pont_Finder/Pont_Finder/servicos/ServicosADM.Designer.cs
+++ Pont_Finder/Pont_Finder/servicos/ServicosADM.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_back = new System.Windows.Forms.PictureBox();
+            this.painel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_back)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 575);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1282, 100);
+            this.panel1.TabIndex = 18;
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.White;
+            this.btn_back.BackgroundImage = global::Pont_Finder.Properties.Resources.back_1;
+            this.btn_back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_back.Location = new System.Drawing.Point(124, 60);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(40, 40);
+            this.btn_back.TabIndex = 17;
+            this.btn_back.TabStop = false;
+            // 
+            // painel
+            // 
+            this.painel.AutoSize = true;
+            this.painel.BackColor = System.Drawing.Color.White;
+            this.painel.Location = new System.Drawing.Point(127, 106);
+            this.painel.MaximumSize = new System.Drawing.Size(1010, 0);
+            this.painel.MinimumSize = new System.Drawing.Size(1010, 0);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(1010, 0);
+            this.painel.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(498, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 37);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Lista de  pedidos";
+            // 
+            // ServicosADM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1282, 675);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.painel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ServicosADM";
             this.Text = "ServicosADM";
+            ((System.ComponentModel.ISupportInitialize)(this.btn_back)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox btn_back;
+        private System.Windows.Forms.Panel painel;
+        private System.Windows.Forms.Label label1;
     }
 }
