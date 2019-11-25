98472d7e97e956da0952ff3acaee708adf73c517
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-11-25 14:55:59 -0300

    sociaList

---
+++ Pont_Finder/Pont_Finder/servicos/socialist/MensagemList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pont_Finder.servicos.socialist
+{
+    class MensagemList
+    {
+        private static List<Mensagem> mensagens = new List<Mensagem>();
+
+        public 
+    }
+}
