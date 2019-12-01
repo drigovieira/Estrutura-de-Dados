f0289457027a3a3b061046f37e513b975ca56e2c
Mateus Silva <lmteussilva@hotmail.com>
2019-12-01 16:27:43 -0300

    +reservar_quarto

---
+++ Pont_Finder/Pont_Finder/hospedagem/Reservar_quarto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder.hospedagem
+{
+    public partial class Reservar_quarto : Form
+    {
+        public Reservar_quarto()
+        {
+            InitializeComponent();
+        }
+
+        private void rb_boleto_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                if (rb_boleto.Checked)
+                {
+                    hospedagem.data.inc.Reserva_boleto boleto = new data.inc.Reserva_boleto();
+                    boleto.Location = new Point(0, 0);
+                    panel_center.Controls.Add(boleto);
+                }
+                else
+                {
+                    panel_center.Controls.Clear();
+                }
+            }
+        }
+
+        private void rb_cartao_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_cartao.Checked)
+            {
+                hospedagem.data.inc.Reserva_cartao cartao = new data.inc.Reserva_cartao();
+                cartao.Location = new Point(0, 0);
+                panel_center.Controls.Add(cartao);
+            }
+            else
+            {
+                panel_center.Controls.Clear();
+            }
+        }
+    }
+}
