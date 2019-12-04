51061d95d819f9220866752a6864c9ae93579416
Miguel <oliveiramiguel142@gmail.com>
2019-12-03 22:24:34 -0300

    Nova mudança tela compras

--- Pont_Finder/Pont_Finder/Boleto/GerarBoleto.Designer.cs
+++ Pont_Finder/Pont_Finder/Boleto/GerarBoleto.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GerarBoleto));
             this.boletoA = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.cpflabel = new System.Windows.Forms.Label();
@@ -42,8 +44,7 @@
             this.btnSalvar = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.boletoA.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnPrint)).BeginInit();
@@ -65,6 +66,26 @@
             this.boletoA.Name = "boletoA";
             this.boletoA.Size = new System.Drawing.Size(1028, 532);
             this.boletoA.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DATA DA COMPRA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DATA DA COMPRA";
             // 
             // label1
             // 
@@ -175,31 +196,21 @@
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
-            // label4
+            // panel1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(3, 259);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 24);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "DATA DA COMPRA";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(3, 340);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(179, 24);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "DATA DA COMPRA";
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 674);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1265, 100);
+            this.panel1.TabIndex = 9;
             // 
             // GerarBoleto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1282, 675);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.boletoA);
@@ -235,5 +246,6 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
---
+++ Pont_Finder/Pont_Finder/eventos/User_Card/Card_boleto.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Pont_Finder.eventos.User_Card
+{
+    partial class Card_boleto
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_txt4_boleto = new System.Windows.Forms.Label();
+            this.mkb_boleto_CPF = new System.Windows.Forms.MaskedTextBox();
+            this.lb_txt3_boleto = new System.Windows.Forms.Label();
+            this.lb_txt2_boleto = new System.Windows.Forms.Label();
+            this.tb_boleto_email = new System.Windows.Forms.TextBox();
+            this.lb_txt1_boleto_ = new System.Windows.Forms.Label();
+            this.mkb_boleto_RG = new System.Windows.Forms.MaskedTextBox();
+            this.lb_txt_boleto_titulo = new System.Windows.Forms.Label();
+            this.tb_boleto_nome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lb_txt4_boleto
+            // 
+            this.lb_txt4_boleto.AutoSize = true;
+            this.lb_txt4_boleto.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_txt4_boleto.Location = new System.Drawing.Point(102, 290);
+            this.lb_txt4_boleto.Name = "lb_txt4_boleto";
+            this.lb_txt4_boleto.Size = new System.Drawing.Size(40, 21);
+            this.lb_txt4_boleto.TabIndex = 35;
+            this.lb_txt4_boleto.Text = "CPF:";
+            // 
+            // mkb_boleto_CPF
+            // 
+            this.mkb_boleto_CPF.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.mkb_boleto_CPF.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mkb_boleto_CPF.Location = new System.Drawing.Point(106, 314);
+            this.mkb_boleto_CPF.Mask = "000.000.000-00";
+            this.mkb_boleto_CPF.Name = "mkb_boleto_CPF";
+            this.mkb_boleto_CPF.Size = new System.Drawing.Size(150, 24);
+            this.mkb_boleto_CPF.TabIndex = 34;
+            this.mkb_boleto_CPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lb_txt3_boleto
+            // 
+            this.lb_txt3_boleto.AutoSize = true;
+            this.lb_txt3_boleto.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_txt3_boleto.Location = new System.Drawing.Point(102, 224);
+            this.lb_txt3_boleto.Name = "lb_txt3_boleto";
+            this.lb_txt3_boleto.Size = new System.Drawing.Size(34, 21);
+            this.lb_txt3_boleto.TabIndex = 33;
+            this.lb_txt3_boleto.Text = "RG:";
+            // 
+            // lb_txt2_boleto
+            // 
+            this.lb_txt2_boleto.AutoSize = true;
+            this.lb_txt2_boleto.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_txt2_boleto.Location = new System.Drawing.Point(102, 163);
+            this.lb_txt2_boleto.Name = "lb_txt2_boleto";
+            this.lb_txt2_boleto.Size = new System.Drawing.Size(57, 21);
+            this.lb_txt2_boleto.TabIndex = 32;
+            this.lb_txt2_boleto.Text = "E-mail:";
+            // 
+            // tb_boleto_email
+            // 
+            this.tb_boleto_email.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_boleto_email.Location = new System.Drawing.Point(106, 187);
+            this.tb_boleto_email.Name = "tb_boleto_email";
+            this.tb_boleto_email.Size = new System.Drawing.Size(415, 24);
+            this.tb_boleto_email.TabIndex = 31;
+            // 
+            // lb_txt1_boleto_
+            // 
+            this.lb_txt1_boleto_.AutoSize = true;
+            this.lb_txt1_boleto_.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_txt1_boleto_.Location = new System.Drawing.Point(102, 95);
+            this.lb_txt1_boleto_.Name = "lb_txt1_boleto_";
+            this.lb_txt1_boleto_.Size = new System.Drawing.Size(125, 21);
+            this.lb_txt1_boleto_.TabIndex = 30;
+            this.lb_txt1_boleto_.Text = "Nome completo:";
+            // 
+            // mkb_boleto_RG
+            // 
+            this.mkb_boleto_RG.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.mkb_boleto_RG.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mkb_boleto_RG.Location = new System.Drawing.Point(106, 248);
+            this.mkb_boleto_RG.Mask = "00.000.000-0";
+            this.mkb_boleto_RG.Name = "mkb_boleto_RG";
+            this.mkb_boleto_RG.Size = new System.Drawing.Size(150, 24);
+            this.mkb_boleto_RG.TabIndex = 29;
+            this.mkb_boleto_RG.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lb_txt_boleto_titulo
+            // 
+            this.lb_txt_boleto_titulo.AutoSize = true;
+            this.lb_txt_boleto_titulo.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_txt_boleto_titulo.Location = new System.Drawing.Point(180, 20);
+            this.lb_txt_boleto_titulo.Name = "lb_txt_boleto_titulo";
+            this.lb_txt_boleto_titulo.Size = new System.Drawing.Size(297, 27);
+            this.lb_txt_boleto_titulo.TabIndex = 28;
+            this.lb_txt_boleto_titulo.Text = "Preencha os dados do boleto";
+            // 
+            // tb_boleto_nome
+            // 
+            this.tb_boleto_nome.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_boleto_nome.Location = new System.Drawing.Point(106, 119);
+            this.tb_boleto_nome.Name = "tb_boleto_nome";
+            this.tb_boleto_nome.Size = new System.Drawing.Size(415, 24);
+            this.tb_boleto_nome.TabIndex = 27;
+            // 
+            // Card_boleto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lb_txt4_boleto);
+            this.Controls.Add(this.mkb_boleto_CPF);
+            this.Controls.Add(this.lb_txt3_boleto);
+            this.Controls.Add(this.lb_txt2_boleto);
+            this.Controls.Add(this.tb_boleto_email);
+            this.Controls.Add(this.lb_txt1_boleto_);
+            this.Controls.Add(this.mkb_boleto_RG);
+            this.Controls.Add(this.lb_txt_boleto_titulo);
+            this.Controls.Add(this.tb_boleto_nome);
+            this.Name = "Card_boleto";
+            this.Size = new System.Drawing.Size(623, 358);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_txt4_boleto;
+        private System.Windows.Forms.MaskedTextBox mkb_boleto_CPF;
+        private System.Windows.Forms.Label lb_txt3_boleto;
+        private System.Windows.Forms.Label lb_txt2_boleto;
+        private System.Windows.Forms.TextBox tb_boleto_email;
+        private System.Windows.Forms.Label lb_txt1_boleto_;
+        private System.Windows.Forms.MaskedTextBox mkb_boleto_RG;
+        private System.Windows.Forms.Label lb_txt_boleto_titulo;
+        private System.Windows.Forms.TextBox tb_boleto_nome;
+    }
+}
