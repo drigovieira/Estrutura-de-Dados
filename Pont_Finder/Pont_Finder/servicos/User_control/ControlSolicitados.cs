9acd46afe123a7eca2b704af3764265191e2ef91
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-12-05 21:21:30 -0300

    solicitados

---
+++ Pont_Finder/Pont_Finder/servicos/User_control/ControlSolicitados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder.servicos.User_control
+{
+    public partial class ControlSolicitados : UserControl
+    {
+        public ControlSolicitados()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt_obter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lb_descricao_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
