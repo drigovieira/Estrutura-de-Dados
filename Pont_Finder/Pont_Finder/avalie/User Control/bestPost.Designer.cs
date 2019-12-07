3d6a9d64a30cafc6cfd7c8d1003b39dffaee93c2
dacolta <57463740+dacolta@users.noreply.github.com>
2019-12-06 22:18:28 -0300

    att avalie card best post
    att avalie card best post

---
+++ Pont_Finder/Pont_Finder/avalie/User Control/bestPost.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Pont_Finder.avalie.User_Control
+{
+    partial class bestPost
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_deslike = new System.Windows.Forms.Label();
+            this.pb_down = new System.Windows.Forms.PictureBox();
+            this.pb_up = new System.Windows.Forms.PictureBox();
+            this.lb_like = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_down)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_up)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_deslike
+            // 
+            this.lb_deslike.AutoSize = true;
+            this.lb_deslike.Font = new System.Drawing.Font("Microsoft PhagsPa", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_deslike.Location = new System.Drawing.Point(208, 124);
+            this.lb_deslike.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_deslike.Name = "lb_deslike";
+            this.lb_deslike.Size = new System.Drawing.Size(18, 20);
+            this.lb_deslike.TabIndex = 39;
+            this.lb_deslike.Text = "0";
+            // 
+            // pb_down
+            // 
+            this.pb_down.BackgroundImage = global::Pont_Finder.Properties.Resources.Deslike_null;
+            this.pb_down.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_down.Image = global::Pont_Finder.Properties.Resources.Deslike_null;
+            this.pb_down.Location = new System.Drawing.Point(150, 113);
+            this.pb_down.Margin = new System.Windows.Forms.Padding(20, 18, 20, 18);
+            this.pb_down.Name = "pb_down";
+            this.pb_down.Size = new System.Drawing.Size(33, 31);
+            this.pb_down.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_down.TabIndex = 38;
+            this.pb_down.TabStop = false;
+            // 
+            // pb_up
+            // 
+            this.pb_up.BackgroundImage = global::Pont_Finder.Properties.Resources.Like_null;
+            this.pb_up.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_up.Image = global::Pont_Finder.Properties.Resources.Like_null;
+            this.pb_up.Location = new System.Drawing.Point(150, 73);
+            this.pb_up.Margin = new System.Windows.Forms.Padding(20, 18, 20, 18);
+            this.pb_up.Name = "pb_up";
+            this.pb_up.Size = new System.Drawing.Size(33, 31);
+            this.pb_up.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_up.TabIndex = 37;
+            this.pb_up.TabStop = false;
+            this.pb_up.Click += new System.EventHandler(this.pb_up_Click);
+            // 
+            // lb_like
+            // 
+            this.lb_like.AutoSize = true;
+            this.lb_like.Font = new System.Drawing.Font("Microsoft PhagsPa", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_like.Location = new System.Drawing.Point(208, 84);
+            this.lb_like.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_like.Name = "lb_like";
+            this.lb_like.Size = new System.Drawing.Size(18, 20);
+            this.lb_like.TabIndex = 36;
+            this.lb_like.Text = "0";
+            this.lb_like.Click += new System.EventHandler(this.lb_like_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "label1";
+            // 
+            // bestPost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_deslike);
+            this.Controls.Add(this.pb_down);
+            this.Controls.Add(this.pb_up);
+            this.Controls.Add(this.lb_like);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "bestPost";
+            this.Size = new System.Drawing.Size(291, 150);
+            this.Load += new System.EventHandler(this.bestPost_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_down)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_up)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_deslike;
+        private System.Windows.Forms.PictureBox pb_down;
+        private System.Windows.Forms.PictureBox pb_up;
+        private System.Windows.Forms.Label lb_like;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Pont_Finder/Pont_Finder/avalie/User Control/bestPost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pont_Finder.avalie.User_Control
+{
+    public partial class bestPost : UserControl
+    {
+        public bestPost()
+        {
+            InitializeComponent();
+        }
+
+        private void bestPost_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_up_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_like_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
