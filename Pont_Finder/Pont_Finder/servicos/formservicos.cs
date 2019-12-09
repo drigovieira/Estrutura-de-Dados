948889ad710577fa7ca84af2ec0992a2f799008f
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-12-08 21:03:34 -0300

    bugfix

--- Pont_Finder/Pont_Finder/servicos/formservicos.cs
+++ Pont_Finder/Pont_Finder/servicos/formservicos.cs
@@ -35,7 +35,7 @@ namespace Pont_Finder.servicos
                 bt_empresa.Text = "Gerenciar Empresa";
        
 
-            pagQuant = 2;
+            pagQuant = 8;
             LoadPosts();
 
 
